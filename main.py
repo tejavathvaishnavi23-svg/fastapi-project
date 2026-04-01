7770c4355c4d5dc88c91d52fccca2b98b7b6c08c
Tejavath Vaishnavi <tejavathvaishnavi23@gmail.com>
2026-04-01 19:38:05 +0530

    Updated main.py with new FastAPI code

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
-    return {"message": "Hello Vaishnavi, FastAPI is working!"}
+    return "Welcome to FastAPI Series !"
 
-@app.get("/about")
-def about():
-    return {"info": "This is about page"}
+
+@app.get("/contact")
+def contact():
+    return "You can connect us any time."
