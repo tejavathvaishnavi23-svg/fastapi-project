7ef6161e5e6922b4485785e5b7e13c4f940d2cbc
Tejavath Vaishnavi <tejavathvaishnavi23@gmail.com>
2026-04-02 19:25:19 +0530

    Updated main.py and added dtos.py and mockData.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from mockData import products
+from dtos import ProductDTO
 
 app = FastAPI()
 
@@ -7,7 +9,66 @@ app = FastAPI()
 def home():
     return "Welcome to FastAPI Series !"
 
+@app.get("/products")
+def get_products():
+    return products
 
-@app.get("/contact")
-def contact():
-    return "You can connect us any time."
+##path params..
+@app.get("/product/{product_id}")
+def get_one_product(product_id:int):
+    ## if product available with the id, return product, else return error message.
+
+    for oneProduct in products:
+        if oneProduct["id"] == product_id:
+            return oneProduct
+
+    return {
+        "error":"Product not found for this ID."
+    }
+
+
+## query params
+@app.get("/greet")
+def greet_user(request: Request):
+    query_params = dict(request.query_params)
+
+    return{
+        "greet": f"Hello {query_params.get("name")} Your age is {query_params.get("age")}"
+    }
+
+
+## body, headers - request headers, Query params
+
+
+## different types of HTTP methods
+
+@app.post("/create_product")
+def create_product(product_data:ProductDTO):
+    product_data = product_data.model_dump()
+    products.append(product_data)
+
+    return {"status":"Product Created Successfully...","data":products}
+
+
+@app.put("/update_product/{product_id}")
+def update_product(product_data:ProductDTO, product_id:int):
+
+    for index, oneProduct in enumerate(products):
+        if oneProduct.get("id") == product_id:
+            products[index] = product_data.model_dump()
+            return {"status":"Product Updated Successfully...","product":product_data}
+
+    return {
+        "error": "Product not found for this ID."
+    }
+
+@app.delete("/delete_product/{product_id}")
+def delete_product(product_id:int):
+    for index, oneProduct in enumerate(products):
+        if oneProduct.get("id") == product_id:
+            deleted_product = products.pop(index)
+            return {"status":"Product Deleted Successfully...", "product":deleted_product}
+
+    return {
+            "error": "Product not found for this ID."
+    }
---
+++ mockData.py
@@ -0,0 +1,14 @@
+products = [
+    {
+        "id": 1,
+        "title":"Mobile Phone",
+        "price":1000,
+        "count":10
+    },
+    {
+        "id": 2,
+        "title":"Apple Macbook",
+        "price":10000,
+        "count":12
+    }
+]
